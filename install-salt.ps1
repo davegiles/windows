9020cfbebc2fe91638ff55123b00639099c8e291
Dave Giles <dgiles@saltstack.com>
2018-06-20 14:26:58 -0400

    Update install-salt.ps1

--- install-salt.ps1
+++ install-salt.ps1
@@ -1,2 +1,2 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install saltminion -y --params '/master=159.89.231.65' --force
+choco install saltminion -y --params '/master=159.89.231.65' --force  --ignore-checksums
